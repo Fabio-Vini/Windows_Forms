109505be05a1818ecf80ba22b844da2bc9af5594
Fabio-Vini <88862357+Fabio-Vini@users.noreply.github.com>
2024-09-02 20:46:48 -0300

    Add files via upload

---
+++ Parte I/aula02/Rendimento_aplicacao/Rendimento_aplicacao/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rendimento_aplicacao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double porcen;
+
+            Console.Write("Digite o valor aplicado: " + "R$");
+            double aplic = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor retirado: " + "R$");
+            double retiro = double.Parse(Console.ReadLine());
+
+            porcen = (retiro - aplic) * (100) / aplic;
+
+            Console.WriteLine("O percentual ganho foi de: " + porcen + "%");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Parte I/aula02/x1_x2_bhaskara/x1_x2_bhaskara/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace x1_x2_bhaskara
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double bhaska, x1, x2;
+
+            Console.Write("Digite o valor de a: ");
+            double a = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor de b: ");
+            double b = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor de c: ");
+            double c = double.Parse(Console.ReadLine());
+
+            bhaska = (Math.Pow(b, 2) - 4 * a * c);
+
+            if (bhaska >= 0)
+            {
+                x1 = (-b + Convert.ToSingle(Math.Sqrt(bhaska))) / (2 * a);
+
+                x2 = (-b - Convert.ToSingle(Math.Sqrt(bhaska))) / (2 * a);
+
+                Console.Write("Delta é igual a: " + bhaska + "\nX1 é igual a: " + x1 + "\nX2 é igual a: " + x2);
+            }
+            else
+            {
+                Console.WriteLine("Resultado negativo! Sem solução");
+            }
+            Console.ReadKey();
+        }
+    }
+}
