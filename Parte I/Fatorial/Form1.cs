9f3fd905fcac37703f18efdb57b56b4031a1f004
Fabio-Vini <88862357+Fabio-Vini@users.noreply.github.com>
2024-09-02 20:58:41 -0300

    Add files via upload

---
+++ Parte I/Fatorial/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fatorial
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double valor = double.Parse(txtValor.Text);
+            double fatorial = valor;
+
+            for (double i = valor - 1; i >= 1; i--)
+            {
+                fatorial = fatorial * i;
+
+                MessageBox.Show("O fatorial é: " + fatorial);
+            }
+        }
+    }
+}
---
+++ Parte I/Termo_n/Termo_n/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace Termo_n
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.valorX = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnExibe = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblSoma = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(19, 197);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(253, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Digite um valor:";
+            // 
+            // valorX
+            // 
+            this.valorX.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.valorX.Location = new System.Drawing.Point(307, 205);
+            this.valorX.Name = "valorX";
+            this.valorX.Size = new System.Drawing.Size(95, 30);
+            this.valorX.TabIndex = 1;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 29;
+            this.listBox1.Location = new System.Drawing.Point(455, 197);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(270, 178);
+            this.listBox1.TabIndex = 2;
+            // 
+            // btnExibe
+            // 
+            this.btnExibe.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExibe.Location = new System.Drawing.Point(27, 255);
+            this.btnExibe.Name = "btnExibe";
+            this.btnExibe.Size = new System.Drawing.Size(375, 48);
+            this.btnExibe.TabIndex = 3;
+            this.btnExibe.Text = "Exibir";
+            this.btnExibe.UseVisualStyleBackColor = true;
+            this.btnExibe.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(22, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(646, 29);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Exibir os “n” primeiros valores da série: : 1/2, 2/3, 3/4, 4/5 ... ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(19, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(535, 29);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Termo geral da série: x/(x+1) onde x = [1,2,3,4 ...]";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(24, 460);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(265, 29);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Soma total dos termos: ";
+            // 
+            // lblSoma
+            // 
+            this.lblSoma.AutoSize = true;
+            this.lblSoma.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSoma.Location = new System.Drawing.Point(307, 460);
+            this.lblSoma.Name = "lblSoma";
+            this.lblSoma.Size = new System.Drawing.Size(0, 29);
+            this.lblSoma.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(737, 525);
+            this.Controls.Add(this.lblSoma);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnExibe);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.valorX);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox valorX;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnExibe;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblSoma;
+    }
+}
+
