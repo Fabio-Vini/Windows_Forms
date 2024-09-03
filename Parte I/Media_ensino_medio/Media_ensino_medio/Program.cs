5b94307f1efc549f68c038dfa06de0a353d77613
Fabio-Vini <88862357+Fabio-Vini@users.noreply.github.com>
2024-09-02 21:04:08 -0300

    Add files via upload

---
+++ Parte I/Media_ensino_medio/Media_ensino_medio/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Media_ensino_medio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double media = 0, soma = 0;
+
+            Console.WriteLine("Agora , digite as quatro notas:");
+
+            for (int i = 1; i < 5; i++)
+            {
+                Console.Write("Digite a nota " + i + ":");
+                double nota = 0;
+
+                double.TryParse(Console.ReadLine(), out nota);
+                //TryParse vai receber o que for digitado e OUT é o comando que diz
+                //para onde vai isso que foi digitado, nesse caso, o que você digitar
+                //vai para a variável 'nota'
+
+                soma += nota;
+            }
+            media = soma / 4;
+            Console.WriteLine("A média de suas notas é: " + media);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Parte I/Media_notas/Media_notas/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Media_notas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float media;
+
+            Console.Write("Digite a nota do 1º bimestre: ");
+            float n1 = float.Parse(Console.ReadLine());
+
+            Console.Write("Digite a nota do 2º bimestre: ");
+            float n2 = float.Parse(Console.ReadLine());
+
+            media = (n1 + n2) / 2;
+
+            Console.Write("A média de sua nota é: " + media);
+
+            Console.ReadKey();
+        }
+    }
+}
