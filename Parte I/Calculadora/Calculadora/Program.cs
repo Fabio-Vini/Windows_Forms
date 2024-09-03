8d7e67bff3837b54ce340cc6a97f1c7faed98106
Fabio-Vini <88862357+Fabio-Vini@users.noreply.github.com>
2024-09-02 21:02:38 -0300

    Add files via upload

---
+++ Parte I/Calculadora/Calculadora/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double calc;
+            
+            inicio:
+
+            Console.Write("Digite um valor: ");
+            double v1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite outro valor: ");
+            double v2 = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite um simbolo de operação para calcular:");
+            Console.Write("\n+ - * /: ");
+            char op = char.Parse(Console.ReadLine());
+
+            if (op == '+')
+            {
+                calc = v1 + v2;
+
+                Console.Write("\nResultado: " + calc);
+                Console.WriteLine("");
+                goto inicio;
+            }
+            else if (op == '-')
+            {
+                calc = v1 - v2;
+
+                Console.Write("\nResultado: " + calc);
+                Console.WriteLine("");
+                goto inicio;
+            }
+            else if (op == '*')
+            {
+                calc = v1 * v2;
+
+                Console.Write("\nResultado: " + calc);
+                Console.WriteLine("");
+                goto inicio;
+            }
+            else if (op == '/')
+            {
+                calc = v1 / v2;
+
+                Console.Write("\nResultado: " + calc);
+                Console.WriteLine("");
+                goto inicio;
+            }
+            else
+            {
+                Console.Write("Opção inválida!");
+                goto inicio;
+                Console.ReadKey();
+            }
+        }
+    }
+}
