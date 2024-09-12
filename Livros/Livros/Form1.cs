7743adb9115ff605722eaba53b914ef0b343f5e5
Fabio-Vini <88862357+Fabio-Vini@users.noreply.github.com>
2024-09-12 10:56:24 -0300

    Add files via upload

---
+++ Livros/Livros/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Livros
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            Livro cadastro = new Livro();
+
+            cadastro.setTitulo(txtTitulo.Text);
+            cadastro.setAutor(txtAutor.Text);
+            cadastro.setEditora(txtEditora.Text);
+            cadastro.setLocal(txtLocal.Text);
+            cadastro.setAnoedit(mskData.Text);
+
+            LivroBLL.validaDados(cadastro);
+            if (Erro.getErro())
+            {
+                MessageBox.Show(Erro.getMens());
+            }
+
+            if (ValidarForm())
+            {
+                MessageBox.Show("Salvo com sucesso!");
+            }
+            else
+            {
+                MessageBox.Show("Preencha todos os campos obrigatórios!");
+            }
+        }
+
+        private bool ValidarForm()
+        {
+            bool FormValido;
+
+            if (txtTitulo.Text == "")
+            {
+                FormValido = false;
+            }
+            else if (txtAutor.Text == "")
+            {
+                FormValido = false;
+            }
+            else if (txtEditora.Text == "")
+            {
+                FormValido = false;
+            }
+            else if (txtLocal.Text == "")
+            {
+                FormValido = false;
+            }
+            else if (!Regex.IsMatch(txtAutor.Text, @"^[a-zA-Z]+$"))
+            {
+                FormValido = false;
+            }
+            else if (!Regex.IsMatch(txtEditora.Text, @"^[a-zA-Z]+$"))
+            {
+                FormValido = false;
+            }
+            else if (!Regex.IsMatch(txtLocal.Text, @"^[a-zA-Z]+$"))
+            {
+                FormValido = false;
+            }
+            else
+
+            FormValido = true;
+            return FormValido;
+        }
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+
+            form2.Titulo = txtTitulo.Text;
+            form2.Autor = txtAutor.Text;
+            form2.Editora = txtEditora.Text;
+            form2.Local = txtLocal.Text;
+            form2.Data = mskData.Text;
+
+
+            if (txtTitulo.Text == "")
+            {
+                MessageBox.Show("Os dados estão incompletos!");
+            }
+            else if (txtAutor.Text == "")
+            {
+                MessageBox.Show("Os dados estão incompletos!");
+            }
+            else if (txtEditora.Text == "")
+            {
+                MessageBox.Show("Os dados estão incompletos!");
+            }
+            else if (txtLocal.Text == "")
+            {
+                MessageBox.Show("Os dados estão incompletos!");
+            }
+            else if (!mskData.MaskCompleted)
+            {
+                MessageBox.Show("Os dados estão incompletos!");
+            }
+            else
+            {
+                form2.Show();
+            }
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtTitulo.Clear();
+            txtAutor.Clear();
+            txtEditora.Clear();
+            txtLocal.Clear();
+            mskData.Clear();
+
+            txtTitulo.Focus();
+        }
+
+        private void btnFechar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ Livros/Livros/Form2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Livros
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            label6.Text = Titulo;
+            label7.Text = Autor;
+            label8.Text = Editora;
+            label9.Text = Local;
+            label11.Text = Data;
+
+        }
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+        public string Editora { get; set; }
+        public string Local { get; set; }
+        public string Data { get; set; }
+
+        private void btnFechar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
