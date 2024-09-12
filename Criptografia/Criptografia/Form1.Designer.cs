ee94f7630ceac43f38d171507ac90b2b4dd1b7b7
Fabio-Vini <88862357+Fabio-Vini@users.noreply.github.com>
2024-09-12 11:06:59 -0300

    Add files via upload

---
+++ Criptografia/Criptografia/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace Criptografia
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtMsg = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtCripto = new System.Windows.Forms.TextBox();
+            this.txtDescr = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtMsg2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(30, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mensagem:";
+            // 
+            // txtMsg
+            // 
+            this.txtMsg.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMsg.Location = new System.Drawing.Point(179, 33);
+            this.txtMsg.Name = "txtMsg";
+            this.txtMsg.Size = new System.Drawing.Size(263, 30);
+            this.txtMsg.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(29, 116);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(173, 38);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Criptografar";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button2.Location = new System.Drawing.Point(26, 309);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(174, 38);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Descriptografar";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtCripto
+            // 
+            this.txtCripto.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCripto.Location = new System.Drawing.Point(208, 120);
+            this.txtCripto.Name = "txtCripto";
+            this.txtCripto.Size = new System.Drawing.Size(234, 30);
+            this.txtCripto.TabIndex = 9;
+            // 
+            // txtDescr
+            // 
+            this.txtDescr.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDescr.Location = new System.Drawing.Point(206, 313);
+            this.txtDescr.Name = "txtDescr";
+            this.txtDescr.Size = new System.Drawing.Size(234, 30);
+            this.txtDescr.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(30, 199);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(160, 58);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "  Mensagem \r\ncriptografada:";
+            // 
+            // txtMsg2
+            // 
+            this.txtMsg2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMsg2.Location = new System.Drawing.Point(206, 227);
+            this.txtMsg2.Name = "txtMsg2";
+            this.txtMsg2.Size = new System.Drawing.Size(234, 30);
+            this.txtMsg2.TabIndex = 12;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Lime;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button3.Location = new System.Drawing.Point(131, 411);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(188, 46);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Limpar";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 573);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.txtMsg2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtDescr);
+            this.Controls.Add(this.txtCripto);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtMsg);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtMsg;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtCripto;
+        private System.Windows.Forms.TextBox txtDescr;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtMsg2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Criptografia_termo_fibonacci/Criptografia_termo_fibonacci/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Criptografia_termo_fibonacci
+{
+    public partial class Form1 : Form
+    {
+        string msgCripto, msgDescripto, cripto, descripto;
+        int aux;
+
+        private int Fibo(int n)
+        {
+            return (n == 1 || n ==2)?1:Fibo(n-1)+Fibo(n-2);
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            cripto = " ";
+            msgCripto = txtMsg.Text;
+            txtMsg.Text = string.Empty;
+
+            for (int i = 0; i < msgCripto.Length; ++i)
+            {
+                aux = (int)msgCripto[i] + Fibo(i + 1);
+                cripto += (char)aux;
+
+                txtCripto.Text = cripto;
+            }
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            descripto = " ";
+            msgDescripto = txtMsg2.Text;
+            txtMsg2.Text = string.Empty;
+
+            for (int i = 0; i < msgDescripto.Length; ++i)
+            {
+                aux = msgDescripto[i] - Fibo(i + 1);
+                descripto += (char)aux;
+
+                txtDescr.Text = descripto;
+            }
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            txtCripto.Clear();
+            txtDescr.Clear();
+        }
+    }
+}
