bf5570553a8fcf857a8e63450383b57baa6c7fc0
Fabio-Vini <88862357+Fabio-Vini@users.noreply.github.com>
2024-09-12 10:57:20 -0300

    Add files via upload

---
+++ Termo_n2.0/Termo_n/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Termo_n
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double soma = 0, valor = 0;
+            int n = int.Parse(valorX.Text);
+
+            for (double x = 1; x <= n; x++)
+            {
+                valor = (Math.Pow(x, 2) + 1) / Math.Pow(x, 3);
+                listBox1.Items.Add(x + "º. " + valor);
+                soma += valor;
+
+                lblSoma.Text = soma.ToString();
+            }
+        }
+    }
+}
