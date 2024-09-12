24e38f3302cbdf8b18e845bf7915d1161e9b4722
Fabio-Vini <88862357+Fabio-Vini@users.noreply.github.com>
2024-09-12 10:58:49 -0300

    Add files via upload

---
+++ Qtd_de_n_primos/Qtd_de_n_primos/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Qtd_de_n_primos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num = 2;
+            int Nprimos = int.Parse(txtQtd.Text);
+
+            if (num < 2)
+            {
+                MessageBox.Show("Sem solucões...");
+            }
+
+            for(int i = 2; i <= Math.Sqrt(num); i++)
+            {
+                if(num % i == 0)
+                {
+                    MessageBox.Show("Sem solucões...");
+                }
+            }
+            return true;
+        }
+    }
+}
