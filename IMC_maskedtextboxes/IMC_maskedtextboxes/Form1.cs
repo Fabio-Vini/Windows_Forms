427a4bd4cc50e9a295d11779c84711e7ebf3a946
Fabio-Vini <88862357+Fabio-Vini@users.noreply.github.com>
2024-09-12 11:04:35 -0300

    Add files via upload

---
+++ IMC_maskedtextboxes/IMC_maskedtextboxes/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IMC_maskedtextboxes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnIMC_Click(object sender, EventArgs e)
+        {
+            double altura = 0, peso = 0, imc = 0;
+
+            altura = double.Parse(mskAltura.Text);
+            peso = double.Parse(mskPeso.Text);
+
+            imc = peso / Math.Pow(altura, 2);
+            lblResultado.Text = imc.ToString();
+
+            if (imc < 20)
+            {
+               /* pictureBox.Image = imageList.Images[1];*/
+                MessageBox.Show("Você está abaixo do peso");
+            }
+            else if (20 <= imc & imc < 25)
+            {
+               /* pictureBox.Image = imageList.Images[2];*/
+                MessageBox.Show("Está no peso ideal!");
+            }
+            else
+            {
+                /*pictureBox.Image = imageList.Images[0];*/
+                MessageBox.Show("Você está acima do peso!");
+            }
+        }
+    }
+}
---
+++ N_aleatorios/N_aleatorios/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace N_aleatorios
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtQtd = new System.Windows.Forms.TextBox();
+            this.lstNumeros = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(32, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(584, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Quantos números aleatórios entre 1 e 100 você quer?";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(146, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(245, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Informe a quantidade:";
+            // 
+            // txtQtd
+            // 
+            this.txtQtd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtQtd.Location = new System.Drawing.Point(396, 91);
+            this.txtQtd.Name = "txtQtd";
+            this.txtQtd.Size = new System.Drawing.Size(100, 30);
+            this.txtQtd.TabIndex = 2;
+            // 
+            // lstNumeros
+            // 
+            this.lstNumeros.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstNumeros.FormattingEnabled = true;
+            this.lstNumeros.ItemHeight = 25;
+            this.lstNumeros.Location = new System.Drawing.Point(150, 204);
+            this.lstNumeros.Name = "lstNumeros";
+            this.lstNumeros.Size = new System.Drawing.Size(347, 204);
+            this.lstNumeros.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(151, 132);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(159, 52);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Calcular";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(337, 134);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(159, 50);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Limpar";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(682, 449);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lstNumeros);
+            this.Controls.Add(this.txtQtd);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtQtd;
+        private System.Windows.Forms.ListBox lstNumeros;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
